bda834d4cd413c707005f4be53249a7d70924d58
Ivan Matsur <Ivan_Matsur@epam.com>
2018-09-21 11:00:55 +0300

    Update "getLoveTrianglesCount" function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var pairsCnt = 0;
+  for(var i = 0; i < preferences.length; i++) {
+    var firstLoves = preferences[i] - 1;
+    var secondLoves = preferences[firstLoves] - 1;
+    var thirdLoves = preferences[secondLoves] - 1;
+
+    if(firstLoves !== secondLoves && secondLoves !== thirdLoves && thirdLoves === i) {
+      pairsCnt++;
+    }
+  }
+
+  return pairsCnt / 3;
 };
